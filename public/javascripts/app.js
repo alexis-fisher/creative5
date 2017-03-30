468e67e9703eebb31ff367fb32ac7135e509f2ec
alexis-fisher <alexisfisher1@gmail.com>
2017-03-30 13:38:30 +0000

    URL Uploading

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,38 +1,66 @@
-angular.module('comment', [])
-.controller('MainCtrl', [
-  '$scope','$http',
-  function($scope,$http){
+var app = angular.module('comment', [])
+
+app.factory('getImage', getImage)
+app.controller('MainCtrl',MainCtrl);
+
+function getImage ($http) {
+  var API_ROOT = 'comments'
+  return {
+    get: function () {
+      return $http
+        .get(API_ROOT)
+        .then(function (resp) {
+          return resp.data
+        })
+    },
+    post: function (formData) {
+      return $http
+        .post(API_ROOT,formData)
+        .then(function (resp) {
+          console.log("Post worked");
+      })
+    }
+  }
+}
+
+function MainCtrl (getImage,$scope,$http){
     $scope.comments = [];
+ 
+    getImage.get()
+       .then(function(data) {
+         $scope.comments = data
+       })    
+
     $scope.addComment = function() {
       var newcomment = {title:$scope.formContent,upvotes:0};
+      getImage.post(newcomment);
+      $scope.comments.push(newcomment);
       $scope.formContent='';
-      $http.post('/comments', newcomment).success(function(data){
-        $scope.comments.push(data);
-      });
-    };
+    }
+
     $scope.upvote = function(comment) {
       return $http.put('/comments/' + comment._id + '/upvote')
         .success(function(data){
           console.log("upvote worked");
           comment.upvotes = data.upvotes;
-        });
-    };
+        })
+    }
 	$scope.incrementUpvotes = function(comment) {
 	  $scope.upvote(comment);
-    };
+    }
     $scope.delete = function(comment) {
       $http.delete('/comments/' + comment._id )
         .success(function(data){
           console.log("delete worked");
-        });
+        })
       $scope.getAll();
-    };
+    }
     $scope.getAll = function() {
       return $http.get('/comments').success(function(data){
         angular.copy(data, $scope.comments);
-      });
-    };
-    $scope.getAll();
-
-  }
-]);
+      })
+    }
+   
+     $scope.getAll();
+   
+}
